7a0c75c1e288be8921c56280e6c0e23bc5dd4b55
EcoClosure <ecosensor@ecoclosure.org>
2024-01-11 10:44:56 -0500

    add useEffect

--- userapp/src/components/DeviceEdit.js
+++ userapp/src/components/DeviceEdit.js
@@ -1,8 +1,18 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Popup from "reactjs-popup";
 const DeviceEdit = ({name, edit, setEdit}) =>{
     const closeModal = () => setEdit(false);
     const [editName, setEditName] = useState(name)
+    const [change, setChange] = useState(false);
+
+    useEffect(()=>{
+        if(change === true){
+            console.log(editName)
+            setChange(false)
+        }
+
+    },[change])
+
     return(
             <Popup open={edit} closeOnDocumentClick onClose={closeModal} className="w-full h-full">
                 <div className="h-full w-full bg-white drop-shadow-lg border p-4">
@@ -10,14 +20,15 @@ const DeviceEdit = ({name, edit, setEdit}) =>{
                         <div className="flex justify-center items-center text-center w-full ">
                             <h1 className="text-xl w-full">{name}</h1>
                         </div>
-                        <form>
+                        <form className="justify-center">
                             <p className="inline-block pr-5">Edit name:</p>
                             <input
                                 onChange={(e) => setEditName(e.target.value)}
                                 type="text"
                                 name="name"
-                                className="border border-slate-500  h-8 w-1/2 "
+                                className="border border-slate-500  h-8 w-1/2"
                             />
+                            <span className="hover:cursor-pointer block" onClick={()=>setChange(true)}>Submit</span>
                         </form>
                     
                 </div>
